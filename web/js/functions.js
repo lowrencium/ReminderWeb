2c7ef1fcaea62dbddf58c3f0339bd0a648db7cf6
Franck Mazzolo <franck.mazzolo@gmail.com>
2014-06-11 00:43:31 +0200

    Disable buttons default behavior

--- web/js/functions.js
+++ web/js/functions.js
@@ -1,2 +1,18 @@
-
+$(document).ready(function(){
+    // Same as the above but automatically stops after two seconds
+    Ladda.bind( 'button[type=submit]', { timeout: 2000 } ); 
+    
+    // Disable default behaviour of submit buttons
+    $('button[type=submit]').on("click", function(e){
+        e.preventDefault();
+    });
+    
+    $("#login").on("click", function(){
+        console.log("login");
+    });
+    
+    $("#do_register").on("click", function(){
+        console.log("do_register");
+    });
+});
 
