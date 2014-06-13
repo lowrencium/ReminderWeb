c597103132eecc2a7774b132c9d085c7209f6200
Amine Amanzou <amine@lacompany.fr>
2014-06-13 06:45:05 +0200

    Adding signin webservice for Android

--- src/App/Controller/UserController.php
+++ src/App/Controller/UserController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Entity\UserRepository;
 use Silex\Application;
 use Silex\ControllerProviderInterface;
 use Symfony\Component\Form\Form;
@@ -48,6 +49,45 @@ class UserController implements ControllerProviderInterface
         return $app['twig']->render('html.twig', array('form' => $form->createView()));
     }
 
+    /**
+     * @param Application $app
+     * @param Request $request
+     * @return mixed
+     */
+    public function signin(Application $app, Request $request)
+    {
+        $response = array("tag" => "login", "success" => 0, "error" => 0);
+        if ($request->isMethod('GET'))
+        {
+            $data = $request->query->all();
+            $user = $app['orm.em']->getRepository('App\Entity\User')
+                    ->findOneByEmail($data['email']);
+            if(empty($user)){
+                $response["error"] = 1;
+                $response["error_msg"] = "Wrong credential";
+            }
+            else {
+                $password = $app['security.encoder_factory']
+                        ->getEncoder($user)
+                        ->encodePassword($data['password'],$user->getSalt());
+
+                if($user->getPassword() == $password){
+                    $response["success"] = 1;
+                    $response["uid"] = "4f074ca1e3df49.06340261";
+                    $response["user"]["name"] = $user->getFirstName();
+                    $response["user"]["email"] = $user->getEmail();
+                    $response["user"]["created_at"] = $user->getCreated()->format('Y-m-d H:i:s');
+                    $response["user"]["updated_at"] = $user->getLastLogin();
+                }
+                else {
+                    $response["error"] = 1;
+                    $response["error_msg"] = "Wrond credential";
+                }
+            }
+        }
+        return new Response(json_encode($response), 200);
+    }
+    
     
     /**
      * @param Application $app
