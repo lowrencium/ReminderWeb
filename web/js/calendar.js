5b373869000a5595386ec27fce0188886e4d3d4e
wajdi aba <wajdi.amorbenali@gmail.com>
2014-06-16 11:38:59 +0200

    Autorefresh

--- web/js/calendar.js
+++ web/js/calendar.js
@@ -52,6 +52,7 @@ $(document).ready(function() {
     $("#addEvent").on("click", function() {
         var button = $("#formAddEvent").find("button[type=submit]");
         buttonBehaviourSubmitDefault(button);
+		
     });
 
     $("#formAddEvent").on("submit", function(e) {
@@ -70,6 +71,7 @@ $(document).ready(function() {
             if (addRappel(idUser, sessionId, title, location, begin, end)) {
                 var message = "Le rappel a été ajouté avec succès";
                 buttonBehaviourSubmitSuccess(button, message);
+				setTimeout(function(){document.location.reload(true)}, 1000);
             }
             else {
                 var error = "Problème lors de l'ajout de l'évènement";
@@ -171,6 +173,7 @@ $(document).ready(function() {
                 if(status){
                     var message = "Rappels partagés avec succès";
                     buttonBehaviourSubmitSuccess(button, message);
+					setTimeout(function(){document.location.reload(true)}, 1000);
                 }
                 else{
                     var error = "Echec du partage";
@@ -216,8 +219,9 @@ $(document).ready(function() {
                 //remove from the DOM
                 if (removeRappel(idUser, sessionId, rappelId)) {
                     event.remove();
-                    var message = "Recharger la page pour avoir le calendrier à jour";
+                    var message = "Supprimé avec succès";
                     buttonBehaviourSubmitSuccess(button, message);
+					setTimeout(function(){document.location.reload(true)}, 1000);
                 }
                 else {
                     var error = "Les rappels n'ont pas pu être supprimés";
